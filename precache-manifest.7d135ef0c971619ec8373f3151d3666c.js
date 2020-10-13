f57da0f8e5794291cf3dd792d20ddb4fa4a61797
albertosilva123 <silvacazaresluis@outlook.com>
2020-10-12 19:33:11 -0500

    Updates

---
+++ precache-manifest.7d135ef0c971619ec8373f3151d3666c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f276e690bb855527e1e675f56efbc685",
+    "url": "/BachesCDMX/index.html"
+  },
+  {
+    "revision": "b611f85ae7bd085074b9",
+    "url": "/BachesCDMX/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "12777c07a884377fed8f",
+    "url": "/BachesCDMX/static/js/2.1fb50390.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BachesCDMX/static/js/2.1fb50390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b611f85ae7bd085074b9",
+    "url": "/BachesCDMX/static/js/main.712c76d8.chunk.js"
+  },
+  {
+    "revision": "67943122f734943a8fa3",
+    "url": "/BachesCDMX/static/js/runtime-main.58d25413.js"
+  }
+]);
